fdf32eac2a3df89ee0a54c71467450a7b156fe7a
SolaceXD <ken918708@gmail.com>
2025-11-05 13:28:40 -0600

    SecondBranch

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const MONGO_URI = "mongodb+srv://Outer:Outer@cluster0.hgrj1r3.mongodb.net/";
 export const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
-    console.log("MongoDB Connected Succesfully!!!!!!!!");
+    console.log("MongoDB Connected Succesfully!!!!!!!! Second Branch");
   } catch (error) {
     console.log("Mongo DB Connected Failed");
   }
