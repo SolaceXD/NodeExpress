ba02805bb4b3e6a40221f86e071d16ca590e6f0f
SolaceXD <ken918708@gmail.com>
2025-11-05 17:21:17 -0600

    ProjectFinished1.4

--- db.js
+++ db.js
@@ -1,12 +1,18 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://Outer:Outer@cluster0.hgrj1r3.mongodb.net/novenoa";
+const MONGO_URI = process.env.MONGO_URI;
 
 export const connectDB = async () => {
   try {
+    if (!MONGO_URI) {
+      throw new Error("Missing MONGO_URI in environment variables");
+    }
+
     await mongoose.connect(MONGO_URI);
-    console.log("MongoDB Connected Successfully!");
+    console.log(" MongoDB Connected Successfully!");
   } catch (error) {
-    console.error("MongoDB Connection Failed:", error);
+    console.error(" MongoDB Connection Failed:", error.message);
   }
 };
